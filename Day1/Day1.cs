cd1257ce065bad5e2513463e39111ad73055983e
mdmubin <md.mubin030@gmail.com>
2022-12-01 23:06:01 +0600

    Solution for Day1

---
+++ Day1/Day1.cs
@@ -0,0 +1,28 @@
+﻿namespace AOC
+{
+    public static class Day1
+    {
+        public static void Main(string[] args)
+        {
+            List<int> calories = new(1250);
+            int elfCalorie = 0;
+            foreach (string line in File.ReadLines("./input.txt"))
+            {
+                bool hasValue = Int32.TryParse(line, out int calorie);
+                if (hasValue)
+                {
+                    elfCalorie += calorie;
+                }
+                else
+                {
+                    calories.Add(elfCalorie);
+                    elfCalorie = 0;
+                }
+            }
+
+            calories.Sort();
+            Console.WriteLine(calories[^1]);
+            Console.WriteLine(calories[^1] + calories[^2] + calories[^3]);
+        }
+    }
+}
