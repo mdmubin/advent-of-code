2c4ecdb4f684928a3c9d432ffd614cf811eceaa0
mdmubin <md.mubin030@gmail.com>
2022-12-05 16:01:01 +0600

    Day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day4;
+
+internal static class Day4
+{
+    private static bool Contained(int[] ranges)
+    {
+        return (ranges[0] <= ranges[2] && ranges[1] >= ranges[3]) || // range 1 contains range 2
+               (ranges[0] >= ranges[2] && ranges[1] <= ranges[3]);   // range 2 contains range 1
+    }
+    private static bool Overlapped(int[] ranges)
+    {
+        return (ranges[0] <= ranges[3] && ranges[3] <= ranges[1]) || // range 1 contains range 2 start
+               (ranges[2] <= ranges[1] && ranges[1] <= ranges[3]);   // range 1 contains range 2 end
+    }
+
+    public static void Main(string[] args)
+    {
+        int contained = 0;
+        int overlapped = 0;
+
+        foreach (string line in File.ReadLines("./input.txt"))
+        {
+            int[] ranges = Array.ConvertAll(Regex.Split(line, @"[,-]"), int.Parse);
+
+            if (Contained(ranges)) contained++;
+            if (Overlapped(ranges)) overlapped++;
+        }
+
+        Console.WriteLine("Part2 Solution: " + contained);
+        Console.WriteLine("Part1 Solution: " + overlapped);
+    }
+}
